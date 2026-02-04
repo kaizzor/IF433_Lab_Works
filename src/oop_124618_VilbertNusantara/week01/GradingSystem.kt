9e110a2f2d6d6054e6718cc9d311aca0e78b93cd
kaizzor <vilbertnusantara18@gmail.com>
2026-02-04 23:01:10 +0700

    week01: implement null safety check

--- src/oop_124618_VilbertNusantara/week01/GradingSystem.kt
+++ src/oop_124618_VilbertNusantara/week01/GradingSystem.kt
@@ -2,19 +2,26 @@ package oop_124618_VilbertNusantara.week01
 
 
 fun calculateStatus(score: Int) = if (score > 75) "Lulus" else "Tidak Lulus"
-fun main(){
-    var name: String ="Jhon Thor"
+fun main() {
+    var name: String = "Jhon Thor"
     val score: Int = 80
 
     println("Nama: $name, Nilai $score ")
 
     val grade = when (score) {
-        in 90.. 100->"A"
-        in 80.. 89->"B"
-        in 70.. 79->"C"
-        else->"D"
+        in 90..100 -> "A"
+        in 80..89 -> "B"
+        in 70..79 -> "C"
+        else -> "D"
+
     }
     println("Grade Kamu: $grade")
 
     println("Status: ${calculateStatus(score)}")
+
+    val studentId:String? =null
+    val idLength = studentId?.length ?: 0
+
+    println("Panjang ID: $idLength")
+
 }
