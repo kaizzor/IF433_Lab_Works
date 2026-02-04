347043064e977614c750ac59eda048e6a1a68c06
kaizzor <vilbertnusantara18@gmail.com>
2026-02-04 23:41:11 +0700

    week01: (tugas 2) implement discount logic

--- src/oop_124618_VilbertNusantara/week01/GameStore.kt
+++ src/oop_124618_VilbertNusantara/week01/GameStore.kt
@@ -1,7 +1,13 @@
 package oop_124618_VilbertNusantara.week01
 
+fun calculateDiscount(price: Int) =
+    if (price > 500000) price * 80 / 100 else price * 90 / 100
+
 fun main() {
 
     val gameTitle = "Cyber Adventure"
     val price = 650000
+
+    val finalPrice = calculateDiscount(price)
 }
+
