48f4413bd75fff45d6b1b92c30cd346ff7f5f84b
kaizzor <vilbertnusantara18@gmail.com>
2026-02-18 15:22:37 +0700

    week03: cleanup code

--- src/oop_124618_VilbertNusantara/week01/week03/Main.kt
+++ src/oop_124618_VilbertNusantara/week01/week03/Main.kt
@@ -2,14 +2,9 @@ package oop_124618_VilbertNusantara.week01.week03
 
 fun main(){
     val e = Employee("Budi");
-
-    //1.Test Validasi Salary
-    e.salary = -1000// Harus Print Error
+    e.salary = -1000
     e.salary = 5000000
     println("Gaji: ${e.salary}")
-    //2. Test Encapsulation
     e.increasePerformance()
-
-    //3. Test Computed Property
     println("Pajak yang harus dibayar: ${e.tax}")
 }
