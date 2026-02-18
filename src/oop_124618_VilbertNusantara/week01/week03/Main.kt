5c26665c9de9294bde835db8fb95c3a0b244ea11
kaizzor <vilbertnusantara18@gmail.com>
2026-02-18 15:24:22 +0700

    week03: (task 1) implement Weapon testing in main

--- src/oop_124618_VilbertNusantara/week01/week03/Main.kt
+++ src/oop_124618_VilbertNusantara/week01/week03/Main.kt
@@ -8,5 +8,8 @@ fun main(){
     e.increasePerformance()
     println("Pajak yang harus dibayar: ${e.tax}")
 
-
+    val w = Weapon("Excalibur")
+    w.damage = -50
+    w.damage = 9999
+    println("Tier Weapon: ${w.tier}")
 }
