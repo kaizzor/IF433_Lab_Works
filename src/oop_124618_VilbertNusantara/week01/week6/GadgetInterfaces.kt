617d4b20488f660ca607e794e1761a77b3d25583
kaizzor <vilbertnusantara18@gmail.com>
2026-03-11 15:05:29 +0700

    week06: create Camera and Phone interfaces with default methods

---
+++ src/oop_124618_VilbertNusantara/week01/week6/GadgetInterfaces.kt
@@ -0,0 +1,12 @@
+package oop_124618_VilbertNusantara.week01.week6
+
+interface Camera {
+    fun turnOn(){
+        println("Lensa Kamera terbuka dan sendor aktif")
+    }
+}
+interface Phone{
+    fun turnOn(){
+        println("Sinyal seluler mencari jaringan.")
+    }
+}
