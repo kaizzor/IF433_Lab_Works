4e415077e719a4b5e54b3c85a5d2b83bb997189f
kaizzor <vilbertnusantara18@gmail.com>
2026-04-15 14:53:43 +0700

    week08: (task) implement payment processing with !!

--- src/oop_124618_VilbertNusantara/week01/week8/ApiParser.kt
+++ src/oop_124618_VilbertNusantara/week01/week8/ApiParser.kt
@@ -27,4 +27,13 @@ class ApiParser {
             else -> null
         }
     }
+    fun checkout(product: Product) {
+        val id = when (product) {
+            is Electronic -> product.id
+            is Clothing -> product.id
+        }
+
+        val trx = JavaPaymentService.processPayment(id)!!
+        println("Transaction ID: $trx")
+    }
 }
