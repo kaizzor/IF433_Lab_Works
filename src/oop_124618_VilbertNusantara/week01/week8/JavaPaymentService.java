5ca3ae2831c92fe1344313fba1968cc1cb28bce5
kaizzor <vilbertnusantara18@gmail.com>
2026-04-15 14:53:15 +0700

    week08: (task) create JavaPaymentService for payment interop

---
+++ src/oop_124618_VilbertNusantara/week01/week8/JavaPaymentService.java
@@ -0,0 +1,7 @@
+package oop_124618_VilbertNusantara.week01.week8;
+
+public class JavaPaymentService {
+    public static String processPayment(String productId) {
+        return "TRX-" + productId + "-SUCCESS";
+    }
+}
