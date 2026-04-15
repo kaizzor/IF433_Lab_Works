8a18a46324c10c9e6499e0249b304bce8076a64a
kaizzor <vilbertnusantara18@gmail.com>
2026-04-15 14:50:50 +0700

    week08: (task) create sealed class Product and data classes

---
+++ src/oop_124618_VilbertNusantara/week01/week8/EcommerceModels.kt
@@ -0,0 +1,15 @@
+package oop_124618_VilbertNusantara.week01.week8
+
+sealed class Product
+
+data class Electronic(
+    val id: String,
+    val name: String,
+    val warrantyMonths: Int
+) : Product()
+
+data class Clothing(
+    val id: String,
+    val name: String,
+    val size: String
+) : Product()
