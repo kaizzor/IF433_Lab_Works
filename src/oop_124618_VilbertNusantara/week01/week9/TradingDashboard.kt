8e1e35da4b69d8c38ee05c35e6721e655ff46759
kaizzor <vilbertnusantara18@gmail.com>
2026-04-22 14:14:42 +0700

    week09: (task) print worst performers and unique pairs

--- src/oop_124618_VilbertNusantara/week01/week9/TradingDashboard.kt
+++ src/oop_124618_VilbertNusantara/week01/week9/TradingDashboard.kt
@@ -34,4 +34,6 @@ fun main() {
     println("=== CRYPTO TRADING DASHBOARD ===")
     topPerformersString.forEach { println(it) }
 
+    worstPerformersString.forEach { println(it) }
+    println("Pairs traded: $uniquePairs")
 }
