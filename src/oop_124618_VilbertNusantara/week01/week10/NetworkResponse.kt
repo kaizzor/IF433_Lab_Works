57173ced2d4d3096880c7ebb67d3d142b23c22c5
kaizzor <vilbertnusantara18@gmail.com>
2026-04-29 14:04:43 +0700

    week10: (task) implement search logic in repository

--- src/oop_124618_VilbertNusantara/week01/week10/NetworkResponse.kt
+++ src/oop_124618_VilbertNusantara/week01/week10/NetworkResponse.kt
@@ -1,3 +1,4 @@
 package oop_124618_VilbertNusantara.week01.week10
 
-data class ApiResponse<T>(val status: String, val data: T)
+data class ApiResponse<T>(val status: String, val data: T);
+
--- src/oop_124618_VilbertNusantara/week01/week10/WalletRepository.kt
+++ src/oop_124618_VilbertNusantara/week01/week10/WalletRepository.kt
@@ -9,5 +9,11 @@ class WalletRepository<T> {
 
     fun getAll(): List<T> {
         return items
+
+    }
+    fun searchByName(name: String): List<T> {
+        return items.filter {
+            it.toString().contains(name, ignoreCase = true)
+        }
     }
 }
