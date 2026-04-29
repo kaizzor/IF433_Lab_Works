170a7bfb6e0e9bc47904e4e013fab4e4e8a3343f
kaizzor <vilbertnusantara18@gmail.com>
2026-04-29 14:05:37 +0700

    week10: (task) populate coin repository

--- src/oop_124618_VilbertNusantara/week01/week10/CryptoDashboard.kt
+++ src/oop_124618_VilbertNusantara/week01/week10/CryptoDashboard.kt
@@ -2,4 +2,8 @@ package oop_124618_VilbertNusantara.week01.week10
 
 fun main() {
     val coinRepo = WalletRepository<Coin>()
+
+    coinRepo.add(Coin("BTC", 1.5))
+    coinRepo.add(Coin("ETH", 10.0))
+    coinRepo.add(Coin("USDT", 5000.0))
 }
