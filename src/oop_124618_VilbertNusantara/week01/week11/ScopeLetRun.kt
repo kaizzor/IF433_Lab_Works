9d1709398948495fa38125ae55b7e4c37aba8a1a
kaizzor <vilbertnusantara18@gmail.com>
2026-05-06 14:42:03 +0700

    week11: implement run function for object computation

--- src/oop_124618_VilbertNusantara/week01/week11/ScopeLetRun.kt
+++ src/oop_124618_VilbertNusantara/week01/week11/ScopeLetRun.kt
@@ -1,16 +1,17 @@
 package oop_124618_VilbertNusantara.week01.week11
 
 fun main() {
+
     println("=== TEST LET FUNCTION ===")
 
     val name: String? = "Alexander"
 
-    val length = name?.let {
+    val nameLength = name?.let {
         println("Nama terdeteksi: $it")
         it.length
     }
 
-    println("Panjang nama: $length")
+    println("Panjang nama: $nameLength")
 
     println("\n=== TEST RUN FUNCTION ===")
 
