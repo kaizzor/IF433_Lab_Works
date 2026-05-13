966e266fb5255e119bc3323b7afcd52cc702c109
kaizzor <vilbertnusantara18@gmail.com>
2026-05-13 14:15:53 +0700

    week12: (task) implement dispenseKibble function

---
+++ src/oop_124618_VilbertNusantara/week01/week12/SmartFeeder.kt
@@ -0,0 +1,14 @@
+package oop_124618_VilbertNusantara.week01.week12
+
+fun dispenseKibble(
+    requestedGram: Int,
+    availableGram: Int,
+    isJammed: Boolean
+): Int {
+
+    require(requestedGram > 0) {
+        "Porsi kibble harus lebih dari 0 gr"
+    }
+
+    return availableGram
+}
