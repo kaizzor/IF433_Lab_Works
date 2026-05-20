53165da5e398426d26ab652e484fb6989a8ce313
kaizzor <vilbertnusantara18@gmail.com>
2026-05-20 13:51:02 +0700

    week13: (task) create TradeRecord data class

---
+++ src/oop_124618_VilbertNusantara/week01/week13/CryptoTradeLogger.kt
@@ -0,0 +1,9 @@
+package oop_124618_VilbertNusantara.week01.week13
+
+data class TradeRecord(
+    val id: Int,
+    val symbol: String,
+    val type: String,
+    val margin: Double,
+    val pnl: Double
+)
