e3215ebe65a59ff2ed4463f798ebf5d6bcde6586
kaizzor <vilbertnusantara18@gmail.com>
2026-05-20 13:49:02 +0700

    week13: create Student model

---
+++ src/oop_124618_VilbertNusantara/week01/week13/StudentSerialization.kt
@@ -0,0 +1,10 @@
+package oop_124618_VilbertNusantara.week01.week13
+
+import java.io.File
+import java.io.FileNotFoundException
+
+data class Student(
+    val name: String,
+    val age: Int,
+    val gpa: Double
+)
